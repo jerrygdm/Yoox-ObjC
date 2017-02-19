52975643a73bd120d8425fd5f48b579b37d7e9e5
Gianmaria Dal Maistro <gianmaria.dalmaistro@icloud.com>
2017-02-19 20:58:13 +0100

    installed pods

---
+++ Yoox/Pods/Target Support Files/AFURLConnectionByteSpeedMeasure/AFURLConnectionByteSpeedMeasure-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "AFURLConnectionByteSpeedMeasure.h"
+#import "AFURLConnectionOperation+AFURLConnectionByteSpeedMeasure.h"
+
+FOUNDATION_EXPORT double AFURLConnectionByteSpeedMeasureVersionNumber;
+FOUNDATION_EXPORT const unsigned char AFURLConnectionByteSpeedMeasureVersionString[];
+
