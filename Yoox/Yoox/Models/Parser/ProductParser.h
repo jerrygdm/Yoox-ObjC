e2f91e168726aca6d9fa6d63acf6e868a1486121
Gianmaria Dal Maistro <gianmaria.dalmaistro@icloud.com>
2017-02-19 21:29:02 +0100

    added parsers

---
+++ Yoox/Yoox/Models/Parser/ProductParser.h
@@ -0,0 +1,15 @@
+//
+//  ProductParser.h
+//  Yoox
+//
+//  Created by Gianmaria Dal Maistro on 17/02/17.
+//  Copyright © 2017 it.whiteworld.yoox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BaseResponseParser.h"
+@protocol ParserProtocol;
+
+@interface ProductParser : BaseResponseParser <ParserProtocol>
+
+@end
